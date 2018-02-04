b39467bafc5f4a7b4cf89529bac2f8c5c7df2c81
RX101 <15041867@myrp.edu.sg>
2018-02-04 11:34:43 +0800

    update profile page

--- app/src/main/java/com/example/angruixian/u_hoo_backend/HomeActivity.java
+++ app/src/main/java/com/example/angruixian/u_hoo_backend/HomeActivity.java
@@ -2,11 +2,17 @@ package com.example.angruixian.u_hoo_backend;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+    Fragment fragment;
+    FragmentManager fm;
+    FragmentTransaction trans;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +22,15 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView bottomBar = (BottomNavigationView) findViewById(R.id.navigation);
         bottomBar.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         BottomNavigationViewHelper.disableShiftMode(bottomBar);
+
+        fragment = new HomeFragment();
+        fm = getSupportFragmentManager();
+        trans = fm.beginTransaction();
+        trans.replace(R.id.mainFrameLayout, fragment);
+        trans.commit();
+
     }
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,14 +39,14 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-//                    fragment = new FragmentHome();
-//                    fm = getSupportFragmentManager();
-//                    trans = fm.beginTransaction();
-//                    trans.replace(R.id.mainFrameLayout, fragment);
-//                    trans.commit();
-//                    return true;
+                    fragment = new HomeFragment();
+                    fm = getSupportFragmentManager();
+                    trans = fm.beginTransaction();
+                    trans.replace(R.id.mainFrameLayout, fragment);
+                    trans.commit();
+                    return true;
 
-                case R.id.navigation_history:
+//                case R.id.navigation_history:
 //                    fragment = new FragmentHistory();
 //                    fm = getSupportFragmentManager();
 //                    trans = fm.beginTransaction();
